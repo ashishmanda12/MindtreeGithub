bbb692380bbed2918621b7ba85d2fd1c87618cf3
ASHISHMANDA <ashishmanda98@gmail.com>
2021-07-27 21:50:25 +0530

    First Commit

---
+++ TestGithub/src/test/java/com/mindtree/testgithub/TestGithubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mindtree.testgithub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestGithubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
